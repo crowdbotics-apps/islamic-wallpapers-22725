65ca456207df3cccfe6f3f6f4b79e624e956af08
Crowdbotics <team@crowdbotics.com>
2020-11-18 12:25:01 +0000

    Initial commit.

---
+++ backend/islamic_wallpapers_22725/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for islamic_wallpapers_22725 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'islamic_wallpapers_22725.settings')
+
+application = get_wsgi_application()
